f1b92d5329e3e0e9a1065d26f49b6122bce6a971
calebsackey <2425402425@live.gctu.edu.gh>
2026-05-31 16:37:14 +0100

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+# File: initials.py
+# Description: Prints stylized large initials for C.N.S. with strict layout constraints.
+# Assignment Number: 1
+#
+# Name: Caleb Nyanney Sackey
+# STUDENT ID: 2425402425
+# Email: 2425402425@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Caleb Nyanney Sackey, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
-
+    # This main function handles the formatted console output for the initials assignment.
+    print()
+    print("...CNS")
+    print()
+    print("...CCCCCCCCCCCC......NNNN....NNNN......SSSSSSSSSSS...")
+    print("...CCCCCCCCCCCC......NNNNN...NNNN......SSSSSSSSSSS...")
+    print("...CCCC..............NNNNNN..NNNN......SSSS..........")
+    print("...CCCC..............NNNN.NN.NNNN......SSSS..........")
+    print("...CCCC..............NNNN..NNNNNN......SSSSSSSSSS....")
+    print("...CCCC..............NNNN...NNNNN......SSSSSSSSSS....")
+    print("...CCCC..............NNNN....NNNN..............SSSS..")
+    print("...CCCC..............NNNN....NNNN..............SSSS..")
+    print("...CCCCCCCCCCCC..**..NNNN....NNNN..**..SSSSSSSSSSS..**")
+    print("...CCCCCCCCCCCC..**..NNNN....NNNN..**..SSSSSSSSSSS..**")
+    print()
 
 
 main()
